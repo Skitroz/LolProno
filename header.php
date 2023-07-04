8db0b2ffbd6fa27b82ae983d5ea4cc958cfeb9b8
Hyuunk <98412595+Hyuunk@users.noreply.github.com>
2023-07-04 23:51:24 +0200

    modification de l'header et .php

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<header>
+    <img id="LoLProno_logo" src="LoLProno logo.png" alt="LoLProno logo">
+    <nav>
+        <ul>        
+            <li class="headerBouton"><a href="index.php"> Accueil </a></li>
+            <li class="headerBouton"><a href="matchdujour.php"> Match du jour </a></li>       
+            <li class="headerBouton"><a href="jeu.php"> Jeu </a></li>
+            <li class="headerBouton"><a href="classement.php"> Classement </a></li>      
+            <li class="headerBouton"><a href="connexion.php"> Connexion </a></li>
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include ("header.php");?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoL Prono</title>
+
+    <link href="style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="LoLProno logo.png">
+
+</head>
+
+<body>
+
+    <h1 id="pageTitle">Coucou</h1>
+
+    <a href="google.com">google</a>
+
+</body>
+</html>
