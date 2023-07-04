ce3e4f80f79f400a3b17a589a560db330e9dd79c
Hyuunk <98412595+Hyuunk@users.noreply.github.com>
2023-07-05 01:08:38 +0200

    header

--- header.php
+++ header.php
@@ -7,13 +7,13 @@
 </head>
 <body>
 <header>
-    <img id="LoLProno_logo" src="LoLProno logo.png" alt="LoLProno logo">
-    <nav>
-        <ul>        
+    <nav id="headerLogo">
+        <ul id="headerBoutonList">   
+            <img id="LoLProno_logo" src="LoLProno logo.png" alt="LoLProno logo">             
             <li class="headerBouton" id="home"><a href="index.php"> Accueil </a></li>
             <li class="headerBouton" id="dailyGames"><a href="matchdujour.php"> Match du jour </a></li>       
             <li class="headerBouton" id="game"><a href="jeu.php"> Jeu </a></li>
-            <li class="headerBouton" id="leaderboard"><a href="classement.php"> Classement </a></li>      
+            <li class="headerBouton" id="leaderboards"><a href="classement.php"> Classements </a></li>      
             <li class="headerBouton" id="login"><a href="connexion.php"> Connexion </a></li>
         </ul>
     </nav>
