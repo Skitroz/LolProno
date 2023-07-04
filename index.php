9802484cf4ac3f52a2404212ade3a765f4493d28
Hyuunk <98412595+Hyuunk@users.noreply.github.com>
2023-07-04 23:55:41 +0200

    reorga

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LoL Prono</title>
 
-    <link href="style.css" rel="stylesheet">
     <link rel="shortcut icon" href="LoLProno logo.png">
 
 </head>
